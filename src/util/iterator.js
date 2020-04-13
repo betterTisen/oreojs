7282119d07c0a6639cf096a9b143a590499496e4
堂铃 <5516023@qq.com>
2020-04-13 10:08:16 +0800

    :pencil2: Modify comments

--- src/util/iterator.js
+++ src/util/iterator.js
@@ -1,16 +1,29 @@
 /**
  * @author byeguo
  * @desc 节点遍历器，递归遍历AST内的每一个节点并调用对应的方法进行解析
- *
+ */
+
+/**
+ * @class
+ * @param object 语法树的json
+ * @param object 当前作用域
+ * @createScope  创建作用域，用于区分父子作用域
+ * @traverse 解析语法树
  */
 
 import NodeHandler from "../es_handle"
+
+import Scope from "./scope"
 
 class NodeIterator {
   constructor(node, scope) {
     this.node = node
     this.scope = scope
     this.nodeHandler = NodeHandler
+  }
+
+  createScope() {
+    return new Scope(this.scope)
   }
 
   traverse(node) {
