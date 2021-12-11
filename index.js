ffdf3398f722b73f85cce47c32c89cbfcb5be636
宋昊天 <5516023@qq.com>
2021-12-11 11:16:10 +0800

    fix: update date

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 import Oreojs from "./src"
 
-new Oreojs(`
-  console.log("hello oreo")
-`).run()
+const str1 = `
+  console.log(1+2)
+`
+
+const str2 = `let a = 2
+function f() {
+  for (let i = 1; i <= 3; i++) {
+    a = a + 1
+    console.log("for:"+i)
+    if(i===2){
+      return a
+    }
+  }
+}
+console.log("end:"+f())`
+
+const str3 = `
+function obj(v){
+  this[a] = v
+}
+const o1 = new obj("o1o1")
+const o2 = new obj("o2o2")
+console.log(o1.a)
+console.log(o2.a)
+o1.a = 3
+console.log(o1.a)
+console.log(o2.a)
+`
+
+new Oreojs(str3).run()
--- src/store/globalMap.js
+++ src/store/globalMap.js
@@ -9,6 +9,7 @@ import SimpleValue from "../util/simpleValue"
 const globalMap = {
   window: new SimpleValue("window", window),
   console: new SimpleValue("console", console),
+  Date: new SimpleValue("Date", Date),
 }
 
 export default globalMap
